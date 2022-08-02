589d7d951d9840af95a2b2d6cdf909fa5c638232
luca-capotombolo <capoluca98@gmail.com>
2022-08-02 15:06:52 +0200

    Inserimento delle malloc()

--- code/simulation.c
+++ code/simulation.c
@@ -4,15 +4,7 @@
 #include "./headers/servicenode4.h"
 #include "./headers/servicenode5.h"
 #include "./headers/rngs.h"
-#include "./headers/randomGeneratorFunctions.h"/*
-#include "./dataStructure/eventList.h"
-#include "./dataStructure/time.h"
-#include "./dataStructure/area.h"
-#include "./dataStructure/stateVariables.h"
-#include "./dataStructure/numArrLoss.h"
-#include "./dataStructure/arrivalTimes.h"
-#include "./dataStructure/utilStructs.h"*/
-
+#include "./headers/randomGeneratorFunctions.h"
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -32,74 +24,134 @@
 #define ABANDON_TIME_1 360
 #define ABANDON_TIME_2 360
 
-struct event_list events;
-struct time t;
-struct area a[5];
-struct state_variables1 sv1[2];		//centers num 1, 3
-struct state_variables2 sv2[3];		//centers num 2, 4, 5
-struct arrival_loss al[5];
-struct arrivals arr;
+struct event_list *events;
+struct time *t;
+struct area *a;
+struct state_variables1 *sv1;		//centers num 1, 3
+struct state_variables2 *sv2;		//centers num 2, 4, 5
+struct arrival_loss *al;
+struct arrivals *arr;
 
+void errorMalloc(int code){
+	printf("Errore malloc...\n");
+	exit(code);
+}
 
+void allocateDataStructure(){
+	events = (struct event_list *) malloc(sizeof(struct event_list));
+	if(events == NULL)
+		errorMalloc(-9);
+		
+	t = (struct time *) malloc(sizeof(struct time));
+	if(t==NULL)
+		errorMalloc(-10);
+		
+	a = (struct area *) malloc(sizeof(struct area)*5);
+	if(a==NULL)
+		errorMalloc(-11);
+		
+	sv1 = (struct state_variables1 *)malloc(sizeof(struct state_variables1) * 2);
+	if(sv1==NULL)
+		errorMalloc(-12);
+		
+	sv2 = (struct state_variables2 *)malloc(sizeof(struct state_variables2)*3);
+	if(sv2==NULL)
+		errorMalloc(-13);
+	
+	
+	al = (struct arrival_loss *) malloc(sizeof(struct arrival_loss)*5);
+	if(al ==NULL)
+		errorMalloc(-14);
+		
+	arr = (struct arrivals *) malloc(sizeof(struct arrivals));
+	if(arr == NULL)
+		errorMalloc(-15);
+		
+	printf("Allocate data structures...\n");
+	fflush(stdout);
+	
+}
 
 void initializeEventList(int *m) {
 
-	events.carArr1.carArrivalTime = getCarArrival(START);
-	events.carArr1.isCarArrivalActive = true;
-	events.familyArr1.familyArrivalTime = getFamilyArrival1(START);
-	events.familyArr1.isFamilyArrivalActive = true;
+	events->carArr1.carArrivalTime = getCarArrival(START);
+	printf("carArrival: %f\n", events->carArr1.carArrivalTime);
+	fflush(stdout);
+	events->carArr1.isCarArrivalActive = true;
+	events->familyArr1.familyArrivalTime = getFamilyArrival1(START);
+	printf("familyArrival: %f\n", events->familyArr1.familyArrivalTime);
+	fflush(stdout);
+	events->familyArr1.isFamilyArrivalActive = true;
 
-	events.familyArr2.familyArrivalTime = getFamilyArrival2(START);
-	events.familyArr2.isFamilyArrivalActive = true;
+	events->familyArr2.familyArrivalTime = getFamilyArrival2(START);
+	printf("familyArrival2 (electr): %f\n", events->familyArr2.familyArrivalTime);
+	fflush(stdout);
+	events->familyArr2.isFamilyArrivalActive = true;
 
-	events.carArr3.carArrivalTime = (double) INFINITY;
-	events.carArr3.isCarArrivalActive = true;
-	events.familyArr3.familyArrivalTime = (double) INFINITY;
-	events.familyArr3.isFamilyArrivalActive = true;
+	events->carArr3.carArrivalTime = (double) INFINITY;
+	events->carArr3.isCarArrivalActive = true;
+	events->familyArr3.familyArrivalTime = (double) INFINITY;
+	events->familyArr3.isFamilyArrivalActive = true;
 
-	events.familyArr4.familyArrivalTime = (double) INFINITY;
-	events.familyArr4.isFamilyArrivalActive = true;
+	events->familyArr4.familyArrivalTime = (double) INFINITY;
+	events->familyArr4.isFamilyArrivalActive = true;
 
-	events.familyArr5.familyArrivalTime = (double) INFINITY;
-	events.familyArr5.isFamilyArrivalActive = true;
+	events->familyArr5.familyArrivalTime = (double) INFINITY;
+	events->familyArr5.isFamilyArrivalActive = true;
 
-	events.head1 = NULL;
-	events.tail1 = NULL;
-	events.head2 = NULL;
-	events.tail2 = NULL;
+	events->head1 = NULL;
+	events->tail1 = NULL;
+	events->head2 = NULL;
+	events->tail2 = NULL;
 
-	events.completionTimes1 = (double *) malloc(sizeof(double)*m[0]);
-	events.completionTimes2 = (double *) malloc(sizeof(double)*m[1]);
-	events.completionTimes3 = (double *) malloc(sizeof(double)*m[2]);
-	events.completionTimes4 = (double *) malloc(sizeof(double)*m[3]);
-	events.completionTimes5 = (double *) malloc(sizeof(double)*m[4]);
+	events->completionTimes1 = (double *) malloc(sizeof(double)*m[0]);
+	if(events->completionTimes1==NULL)
+		errorMalloc(-16);
+	events->completionTimes2 = (double *) malloc(sizeof(double)*m[1]);
+	if(events->completionTimes2==NULL)
+		errorMalloc(-17);
+	events->completionTimes3 = (double *) malloc(sizeof(double)*m[2]);
+	if(events->completionTimes3==NULL)
+		errorMalloc(-18);
+	events->completionTimes4 = (double *) malloc(sizeof(double)*m[3]);
+	if(events->completionTimes4==NULL)
+		errorMalloc(-19);
+	events->completionTimes5 = (double *) malloc(sizeof(double)*m[4]);
+	if(events->completionTimes5==NULL)
+		errorMalloc(-20);
 
 	for(int i=0; i<m[0]; i++) {
-		events.completionTimes1[i] = (double) INFINITY;
+		events->completionTimes1[i] = (double) INFINITY;
 	}
 	for(int i=0; i<m[1]; i++) {
-		events.completionTimes2[i] = (double) INFINITY;
+		events->completionTimes2[i] = (double) INFINITY;
 	}
 	for(int i=0; i<m[2]; i++) {
-		events.completionTimes3[i] = (double) INFINITY;
+		events->completionTimes3[i] = (double) INFINITY;
 	}
 	for(int i=0; i<m[3]; i++) {
-		events.completionTimes4[i] = (double) INFINITY;
+		events->completionTimes4[i] = (double) INFINITY;
 	}
 	for(int i=0; i<m[4]; i++) {
-		events.completionTimes5[i] = (double) INFINITY;
+		events->completionTimes5[i] = (double) INFINITY;
 	}
+	
+	printf("Event list...\n");
+	fflush(stdout);
 
 
 }
 
 void initializeTime() {
 
-	t.current = 0.0;
-	t.next = 0.0;
+	t->current = 0.0;
+	t->next = 0.0;
 	for(int i=0; i<5; i++) {
-		t.last[i] = 0.0;
+		t->last[i] = 0.0;
 	}
+	
+	printf("Time...\n");
+	fflush(stdout);
 
 }
 
@@ -110,6 +162,8 @@ void initializeArea() {
 		a[i].queue = 0.0;
 		a[i].service = 0.0;
 	}
+	printf("Area...\n");
+	fflush(stdout);
 
 }
 
@@ -118,12 +172,22 @@ void initializeStateVariables(int *m) {
 	sv1[0].qA = 0;
 	sv1[0].qF = 0;
 	sv1[0].x = (int *) malloc(sizeof(int)*m[0]);
+	if(sv1[0].x==NULL){
+		printf("Errore malloc...\n");
+		fflush(stdout);
+		exit(-4);
+	}
 	for(int i=0; i<m[0]; i++) {
 		sv1[0].x[i] = 0;	//0=IDLE, 1=BUSY_F, 2=BUSY_A
 	}
 
 	sv2[0].l = 0;
 	sv2[0].x = (int *) malloc(sizeof(int)*m[1]);
+	if(sv2[0].x==NULL){
+		printf("Errore malloc...\n");
+		fflush(stdout);
+		exit(-5);
+	}
 	for(int i=0; i<m[1]; i++) {
 		sv2[0].x[i] = 0;	//0=IDLE, 1=BUSY
 	}
@@ -131,21 +195,38 @@ void initializeStateVariables(int *m) {
 	sv1[1].qA = 0;
 	sv1[1].qF = 0;
 	sv1[1].x = (int *) malloc(sizeof(int)*m[2]);
+	if(sv1[1].x==NULL){
+		printf("Errore malloc...\n");
+		fflush(stdout);
+		exit(-6);
+	}
 	for(int i=0; i<m[2]; i++) {
 		sv1[1].x[i] = 0;	//0=IDLE, 1=BUSY_F, 2=BUSY_A
 	}
 
 	sv2[1].l = 0;
 	sv2[1].x = (int *) malloc(sizeof(int)*m[3]);
+	if(sv2[1].x==NULL){
+		printf("Errore malloc...\n");
+		fflush(stdout);
+		exit(-7);
+	}
 	for(int i=0; i<m[3]; i++) {
 		sv2[1].x[i] = 0;	//0=IDLE, 1=BUSY
 	}
 
 	sv2[2].l = 0;  			//unuseful
 	sv2[2].x = (int *) malloc(sizeof(int)*m[4]);
+	if(sv2[2].x==NULL){
+		printf("Errore malloc...\n");
+		fflush(stdout);
+		exit(-8);
+	}
 	for(int i=0; i<m[4]; i++) {
 		sv2[2].x[i] = 0;	//0=IDLE, 1=BUSY
 	}
+	printf("State Variables...\n");
+	fflush(stdout);
 
 }
 
@@ -156,19 +237,24 @@ void initializeArrivalLoss() {
 		al[i].index_f = 0;
 		al[i].numLoss_f = 0;
 	}
+	printf("Arrival loss...\n");
+	fflush(stdout);
 
 }
 
 void initializeArrivals() {
 
-	arr.head3 = NULL;
-	arr.tail3 = NULL;
+	arr->head3 = NULL;
+	arr->tail3 = NULL;
 	
-	arr.head4 = NULL;
-	arr.tail4 = NULL;
+	arr->head4 = NULL;
+	arr->tail4 = NULL;
 	
-	arr.head5 = NULL;
-	arr.tail5 = NULL;
+	arr->head5 = NULL;
+	arr->tail5 = NULL;
+	
+	printf("Arrivals...\n");
+	fflush(stdout);
 
 }
 
@@ -238,10 +324,10 @@ struct next_completion *getMinCompletion(int numServers, int *x) {
 	min->completionTime = (double) INFINITY;
 
 	for(int i=0; i<numServers; i++) {
-		if(events.completionTimes1[i] < min->completionTime) {
+		if(events->completionTimes1[i] < min->completionTime) {
 			min->serverOffset = i;
 			min->isFamily = (bool) (x[i]==1);
-			min->completionTime = events.completionTimes1[i];
+			min->completionTime = events->completionTimes1[i];
 		}
 	}
 
@@ -270,13 +356,13 @@ double getMinimumTime(int *m) {
 	struct next_abandon *nextAb2 = NULL;
 	int len = 14;
 	
-	if(events.head1 != NULL) {
+	if(events->head1 != NULL) {
 		//almeno un job che deve abbandonare
-		struct next_abandon *nextAb1 = getMinAbandon(events.head1);
+		struct next_abandon *nextAb1 = getMinAbandon(events->head1);
 		minAbandon1 = nextAb1->abandonTime;
 	}
-	if(events.head2 != NULL) {
-		struct next_abandon *nextAb2 = getMinAbandon(events.head2);
+	if(events->head2 != NULL) {
+		struct next_abandon *nextAb2 = getMinAbandon(events->head2);
 		minAbandon2 = nextAb2->abandonTime;
 	}
 
@@ -301,13 +387,13 @@ double getMinimumTime(int *m) {
 	timesToCompare[4] = minService3;
 	timesToCompare[5] = minService4;
 	timesToCompare[6] = minService5;
-	timesToCompare[7] = events.carArr1.carArrivalTime;
-	timesToCompare[8] = events.familyArr1.familyArrivalTime;
-	timesToCompare[9] = events.familyArr2.familyArrivalTime;
-	timesToCompare[10] = events.carArr3.carArrivalTime;
-	timesToCompare[11] = events.familyArr3.familyArrivalTime;
-	timesToCompare[12] = events.familyArr4.familyArrivalTime;
-	timesToCompare[13] = events.familyArr5.familyArrivalTime;
+	timesToCompare[7] = events->carArr1.carArrivalTime;
+	timesToCompare[8] = events->familyArr1.familyArrivalTime;
+	timesToCompare[9] = events->familyArr2.familyArrivalTime;
+	timesToCompare[10] = events->carArr3.carArrivalTime;
+	timesToCompare[11] = events->familyArr3.familyArrivalTime;
+	timesToCompare[12] = events->familyArr4.familyArrivalTime;
+	timesToCompare[13] = events->familyArr5.familyArrivalTime;
 
 	free(nextAb1);
 	free(nextAb2);
@@ -335,14 +421,20 @@ int countBusyServers(int numServers, int *serverList) {
 
 int main(int argc, char **argv){
 
-	if(argc < 7)
+	if(argc < 7){
+		printf("errore nei parametri...\n");
+		fflush(stdout);
 		exit(-1);
+	}
 
 	int* m = NULL;
 	m = (int *)malloc(sizeof(int)*(5));
 
-	if(m==NULL)
+	if(m==NULL){
+		printf("Errore malloc...\n");
+		fflush(stdout);
 		exit(-2);
+	}
 
 	m[0] = atoi(argv[1]);
 	m[1] = atoi(argv[2]);
@@ -372,103 +464,124 @@ int main(int argc, char **argv){
 			interTime = 180.0;
 			break;
 		default:
+			printf("Errore nello switch...\n");
+			fflush(stdout);
 			exit(-3);
 	}
+	
+	//printf("%f\n", interTime);
+	//printf("%d %d %d %d %d\n", m[0], m[1], m[2], m[3], m[4]);
+	//fflush(stdout);
+	
 
-	initializeTime();
+	allocateDataStructure();
+	initializeTime();	
 	initializeArea();
 	initializeStateVariables(m);
 	initializeArrivalLoss();
 	initializeArrivals();
+	
 
 	PlantSeeds(7000);
+	
+	//printf("PlanSeeds...\n");
+	//fflush(stdout);
+	
+	
 	initializeEventList(m);
+	
+	
 
-	while(events.carArr1.isCarArrivalActive || events.familyArr1.isFamilyArrivalActive || events.familyArr2.isFamilyArrivalActive || !isSystemEmpty(m)) {
+	while(events->carArr1.isCarArrivalActive || events->familyArr1.isFamilyArrivalActive || events->familyArr2.isFamilyArrivalActive || !isSystemEmpty(m)) {
 
-		t.next = getMinimumTime(m);		//Next event time
+		t->next = getMinimumTime(m);		//Next event time
+		
+		printf("next occurence: %f\n", t->next);
+		fflush(stdout);
+		
+		return 0;
 		
 		int xBusy1 = countBusyServers(m[0], sv1[0].x);
-		a[0].service += (t.next - t.current)*xBusy1;
-		a[0].queue += (t.next - t.current)*(sv1[0].qA + sv1[0].qF);
-		a[0].node += (t.next - t.current)*(sv1[0].qA + sv1[0].qF + xBusy1);
+		a[0].service += (t->next - t->current)*xBusy1;
+		a[0].queue += (t->next - t->current)*(sv1[0].qA + sv1[0].qF);
+		a[0].node += (t->next - t->current)*(sv1[0].qA + sv1[0].qF + xBusy1);
 
 		int xBusy2 = countBusyServers(m[1], sv2[0].x);
-		a[1].service += (t.next - t.current)*xBusy2;
-		a[1].queue += (t.next - t.current)*(sv2[0].l - xBusy2);
-		a[1].node += (t.next - t.current)*(sv2[0].l);
+		a[1].service += (t->next - t->current)*xBusy2;
+		a[1].queue += (t->next - t->current)*(sv2[0].l - xBusy2);
+		a[1].node += (t->next - t->current)*(sv2[0].l);
 
 		int xBusy3 = countBusyServers(m[2], sv1[1].x);
-		a[2].service += (t.next - t.current)*xBusy3;
-		a[2].queue += (t.next - t.current)*(sv1[1].qA + sv1[1].qF);
-		a[2].node += (t.next - t.current)*(sv1[1].qA + sv1[1].qF + xBusy3);
+		a[2].service += (t->next - t->current)*xBusy3;
+		a[2].queue += (t->next - t->current)*(sv1[1].qA + sv1[1].qF);
+		a[2].node += (t->next - t->current)*(sv1[1].qA + sv1[1].qF + xBusy3);
 
 		int xBusy4 = countBusyServers(m[3], sv2[1].x);
-		a[3].service += (t.next - t.current)*xBusy4;
-		a[3].queue += (t.next - t.current)*(sv2[1].l - xBusy4);
-		a[3].node += (t.next - t.current)*(sv2[1].l);
+		a[3].service += (t->next - t->current)*xBusy4;
+		a[3].queue += (t->next - t->current)*(sv2[1].l - xBusy4);
+		a[3].node += (t->next - t->current)*(sv2[1].l);
 
 		int xBusy5 = countBusyServers(m[4], sv2[2].x);
-		a[4].service += (t.next - t.current)*xBusy5;
-		a[4].node += (t.next - t.current)*xBusy5;
+		a[4].service += (t->next - t->current)*xBusy5;
+		a[4].node += (t->next - t->current)*xBusy5;
 
-		t.current = t.next;		//Clock update
+		t->current = t->next;		//Clock update
 
-		struct next_abandon *nextAb1 = getMinAbandon(events.head1);
-		struct next_abandon *nextAb2 = getMinAbandon(events.head2);
+		struct next_abandon *nextAb1 = getMinAbandon(events->head1);
+		struct next_abandon *nextAb2 = getMinAbandon(events->head2);
 		struct next_completion *nextCom1 = getMinCompletion(m[1], sv1[0].x);
 		struct next_completion *nextCom2 = getMinCompletion(m[2], sv2[0].x);
 		struct next_completion *nextCom3 = getMinCompletion(m[3], sv1[1].x);
 		struct next_completion *nextCom4 = getMinCompletion(m[4], sv2[1].x);
 		struct next_completion *nextCom5 = getMinCompletion(m[5], sv2[2].x);
 
-		if(t.current == events.carArr1.carArrivalTime) {
-			carArrival1(&events, &t, &sv1[0], &al[0], m[0]);
+		if(t->current == events->carArr1.carArrivalTime) {
+			carArrival1(events, t, &sv1[0], &al[0], m[0]);
 		}
-		else if(t.current == events.familyArr1.familyArrivalTime) {
-			familyArrival1(&events, &t, &sv1[0], &al[0], m[0]);
+		else if(t->current == events->familyArr1.familyArrivalTime) {
+			familyArrival1(events, t, &sv1[0], &al[0], m[0]);
 		}
-		else if(t.current == nextCom1->completionTime && !nextCom1->isFamily) {
-			carDeparture1(&events, &t, &sv1[0], &arr, nextCom1->serverOffset);
+		else if(t->current == nextCom1->completionTime && !nextCom1->isFamily) {
+			carDeparture1(events, t, &sv1[0], arr, nextCom1->serverOffset);
 		}
-		else if(t.current == nextCom1->completionTime && nextCom1->isFamily) {
-			familyDeparture1(&events, &t, &sv1[0], &arr, nextCom1->serverOffset, m[0]);
+		else if(t->current == nextCom1->completionTime && nextCom1->isFamily) {
+			familyDeparture1(events, t, &sv1[0], arr, nextCom1->serverOffset, m[0]);
 		}
-		else if(t.current == nextAb1->abandonTime) {
-			abandon1(&events, &sv1[0], &al[0], nextAb1->jobId);
+		else if(t->current == nextAb1->abandonTime) {
+			abandon1(events, &sv1[0], &al[0], nextAb1->jobId);
 		}
-		else if(t.current == events.familyArr2.familyArrivalTime) {
-			arrival2(&events, &t, &sv2[0], &al[1], m[1]);
+		else if(t->current == events->familyArr2.familyArrivalTime) {
+			arrival2(events, t, &sv2[0], &al[1], m[1]);
 		}
-		else if(t.current == nextCom2->completionTime) {
-			departure2(&events, &t, &sv2[0], &arr, nextCom2->serverOffset);
+		else if(t->current == nextCom2->completionTime) {
+			departure2(events, t, &sv2[0], arr, nextCom2->serverOffset);
 		}
-		else if(t.current == nextAb2->abandonTime) {
-			abandon2(&events, &sv2[0], &al[1], nextAb2->jobId);
+		else if(t->current == nextAb2->abandonTime) {
+			abandon2(events, &sv2[0], &al[1], nextAb2->jobId);
 		}
-		else if(t.current == events.carArr3.carArrivalTime) {
-			carArrival3(&events, &t, &sv1[1], &al[2], &arr, m[2]);
+		else if(t->current == events->carArr3.carArrivalTime) {
+			carArrival3(events, t, &sv1[1], &al[2], arr, m[2]);
 		}
-		else if(t.current == events.familyArr3.familyArrivalTime) {
-			familyArrival3(&events, &t, &sv1[1], &al[2], &arr, m[2]);
+		else if(t->current == events->familyArr3.familyArrivalTime) {
+			familyArrival3(events, t, &sv1[1], &al[2], arr, m[2]);
 		}
-		else if(t.current == nextCom3->completionTime && !nextCom3->isFamily) {
-			carDeparture3(&events, &t, &sv1[1], &arr, nextCom3->serverOffset);
+		else if(t->current == nextCom3->completionTime && !nextCom3->isFamily) {
+			carDeparture3(events, t, &sv1[1], arr, nextCom3->serverOffset);
 		}
-		else if(t.current == nextCom3->completionTime && nextCom3->isFamily) {
-			familyDeparture3(&events, &t, &sv1[1], &arr, nextCom3->serverOffset, m[2]);
+		else if(t->current == nextCom3->completionTime && nextCom3->isFamily) {
+			familyDeparture3(events, t, &sv1[1], arr, nextCom3->serverOffset, m[2]);
 		}
-		else if(t.current == events.familyArr4.familyArrivalTime) {
-			arrival4(&events, &t, &sv2[1], &al[3], &arr, m[3]);
+		else if(t->current == events->familyArr4.familyArrivalTime) {
+			arrival4(events, t, &sv2[1], &al[3], arr, m[3]);
 		}
-		else if(t.current == nextCom4->completionTime) {
-			departure4(&events, &t, &sv2[1], &arr, nextCom4->serverOffset);
+		else if(t->current == nextCom4->completionTime) {
+			departure4(events, t, &sv2[1], arr, nextCom4->serverOffset);
 		}
-		else if(t.current == events.familyArr5.familyArrivalTime) {
-			arrival5(&events, &t, &sv2[2], &al[4], &arr, m[4]);
+		else if(t->current == events->familyArr5.familyArrivalTime) {
+			arrival5(events, t, &sv2[2], &al[4], arr, m[4]);
 		}
-		else if(t.current == nextCom5->completionTime) {
-			departure5(&events, &sv2[2], nextCom5->serverOffset);
+		else if(t->current == nextCom5->completionTime) {
+			departure5(events, &sv2[2], nextCom5->serverOffset);
 		}
 
 		free(nextAb1);
